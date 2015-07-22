c8abd2c533baf87e220c70717e5515d63fd03cd7
Danny <danielbsacks@gmail.com>
2015-07-21 21:04:05 -0700

    update

--- rough_draft.py
+++ rough_draft.py
@@ -9,6 +9,8 @@ Created on Mon Jul 20 21:06:35 2015
 import pandas as pd
 import numpy as np
 
+import seaborn as sns
+
 # read in the CSV file from a URL
 ncaab = pd.read_csv('https://raw.githubusercontent.com/dansnacks/SF_DAT_15_WORK/master/project_data.csv')
 
@@ -16,12 +18,28 @@ ncaab.dropna()
 
 ncaab[['Player', 'BPG', 'Drafted']][ncaab.Position=='C'].sort_index(by='Drafted')
 
+# explore data numerically, looking for differences between species
+ncaab.describe()
+ncaab['Drafted' == 'Y'].describe()
+ncaab.groupby('Position').Height.mean().sort_index(by='Height')
+iris.groupby('species')['sepal_length', 'sepal_width', 'petal_length', 'petal_width'].mean()
+iris.groupby('species').describe()
+
+#Average height by position
+ncaab.groupby('Position', as_index=False).Height.mean().sort_index(by='Height', ascending = False)
+
+#Average height by position for drafted players
+ncaab[['Height', 'Position']][ncaab['Drafted'] == 'Y'].groupby('Position', as_index=False).mean().sort_index(by='Height', ascending = False)
+
+# Plot histograms of wine servings by continent, 
+# remember to share x and share y axis scales!
+ncaab.Height.hist(by=ncaab.Position)
 
 def color_drafted(yesno):
     if yesno == 'Y':
-        return 'b'
-    else:
         return 'r'
+    else:
+        return 'k'
 
 colors = ncaab.Drafted.apply(color_drafted)
 colors
@@ -31,15 +49,22 @@ ncaab.plot(x='Height', y='Weight', kind='scatter', alpha=0.3, c=colors)
 def color_position(position):
     if position == 'C':
         return 'r'
-    elif position == 'PG' or position == 'SG' or position == 'G':
-        return 'b'
     elif position == 'PF' or position == 'SF' or position == 'F':        
         return 'g'    
+    elif position == 'PG' or position == 'SG' or position == 'G':
+        return 'b'
     else:
         return 'y'
 
 colorposition = ncaab.Position.apply(color_position)
 ncaab.plot(x='Height', y='Weight', kind='scatter', alpha=0.3, c=colorposition)
+
+
+
+fig, axs = plt.subplots(1, 3, sharey=True)
+ncaab.plot(kind='scatter', x='Height', y='Weight', ax=axs[0], figsize=(16, 6))
+data.plot(kind='scatter', x='Radio', y='Sales', ax=axs[1])
+data.plot(kind='scatter', x='Newspaper', y='Sales', ax=axs[2])
 
 
 '''
@@ -56,10 +81,17 @@ sns.pairplot(data, x_vars=['TV','Radio','Newspaper'], y_vars='Sales', size=4.5, 
 
 # include a "regression line"
 sns.pairplot(data, x_vars=['TV','Radio','Newspaper'], y_vars='Sales', size=4.5, aspect=0.7, kind='reg')
-
-
-sns.pairplot(ncaab)
 '''
+columns= ncaab.columns
+sns.pairplot(ncaab[[ncaab.columns]][ncaab['Drafted'] == 'Y'], kind='reg')
+sns.heatmap(ncaab.corr())
+
+
+#heatmap for Drafted Y/N - next three lines
+df=pd.DataFrame(ncaab)
+g = sns.FacetGrid(df, col='Drafted')
+g.map_dataframe(lambda data, color: sns.heatmap(data.corr(), linewidths=0))
+
 
 '''
 from pandas.tools.plotting import parallel_coordinates
@@ -71,3 +103,19 @@ iris_df['Name'] = iris.target_names[iris.target]
 parallel_coordinates(data=iris_df, class_column='Name', 
                      colors=('#FF0054', '#FBD039', '#23C2BC'))
 '''
+ncaab.Position
+
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import parallel_coordinates
+features = [name for name in ncaab.columns]
+ncaab_df = pd.DataFrame(ncaab, columns = features)
+ncaab_df['Position'] = ncaab.target_names[ncaab.target]
+parallel_coordinates(data=ncaab_df, class_column='Position', 
+                     colors=('#FF0054', '#FBD039', '#23C2BC'))
+
+plt.figure()
+parallel_coordinates(data, 'Name')
+
+
+, color=['r','g','b','m','y','c','k','w','eeefff']
+
